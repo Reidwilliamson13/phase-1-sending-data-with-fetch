e5a2e5324b11a026697e49a7b6379becb8779f25
ReidWilliamson <reidwilliamson2@gmail.com>
2021-10-21 19:13:03 -0400

    commit tree 849b6a7b5e69efa2b30fa76d83fe2c60aa1624b2

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+
+const body = document.getElementsByTagName("body")[0];
+console.log(body);
+
+function submitData(firstName, email) {
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: firstName,
+      email: email,
+    }),
+  };
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then((response) => response.json())
+    .then((body) => addNewContact(body.id))
+    .catch(errorfunc);
+}
+
+function addNewContact(newID) {
+  document.getElementsByTagName("body")[0].innerHTML = newID;
+}
+submitData("Name", "name@name.com");
+
+function errorfunc(error) {
+  document.body.innerHTML = error.message;
+}
